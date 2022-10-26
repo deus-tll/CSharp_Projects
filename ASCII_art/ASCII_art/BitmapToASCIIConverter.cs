87ad589826cbcede5176d871db3d7331d562356a
deus-tll <millerartemka14@gmail.com>
2022-10-26 16:38:25 +0300

    update

---
+++ ASCII_art/ASCII_art/BitmapToASCIIConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ASCII_art
+{
+	public class BitmapToASCIIConverter
+	{
+		private readonly char[] _asciitable = { '.', ',', ':', '+', '*', '?', '%', 'S', '#', '@' };
+		private readonly Bitmap _bitmap;
+
+		public BitmapToASCIIConverter(Bitmap bitmap)
+		{
+			_bitmap = bitmap;
+		}
+
+
+		public char[][] Convert()
+		{
+			return Convert(_asciitable);
+		}
+
+
+		public char[][] ConvertAsNegative()
+		{
+			return Convert(_asciitable.Reverse().ToArray());
+		}
+
+
+		private char[][] Convert(char[] asciitable)
+		{
+			var result = new char[_bitmap.Height][];
+
+			
+			for (int y = 0; y < _bitmap.Height; ++y)
+			{
+				result[y] = new char[_bitmap.Width];
+				for (int x = 0; x < _bitmap.Width; ++x)
+				{
+					int mapIndex = (int)Map(_bitmap.GetPixel(x, y).R, 0, 255, 0, asciitable.Length - 1);
+					result[y][x] = asciitable[mapIndex];
+				}
+			}
+
+
+			return result;
+		}
+
+
+		private float Map(float valueToMap, float start1, float stop1, float start2, float stop2)
+		{
+			return ((valueToMap - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+		}
+	}
+}
---
+++ ASCII_art/ASCII_art/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace ASCII_art
+{
+	internal class Program
+	{
+		private const double WIDTH_OFFSET = 1.7;
+		private const int MAX_WIDTH = 474;
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Console.BackgroundColor = ConsoleColor.Black;
+			Console.ForegroundColor = ConsoleColor.Cyan;
+
+			var openFileDialog = new OpenFileDialog()
+			{
+				Filter = "Images | *.bmp; *.png; *.jpg; *,JPEG"
+			};
+
+			Console.WriteLine("Press Enter to start...\n");
+
+			while (true)
+			{
+				Console.ReadLine();
+
+
+				if (openFileDialog.ShowDialog() != DialogResult.OK)
+					continue;
+
+				Console.Clear();
+
+
+				var bitmap = new Bitmap(openFileDialog.FileName);
+				bitmap = ResizeBitmap(bitmap);
+				bitmap.ToGrayScale();
+
+
+				var converter = new BitmapToASCIIConverter(bitmap);
+				var rows = converter.Convert();
+
+
+				foreach (var row in rows)
+					Console.WriteLine(row);
+
+				//var rowsNegative = converter.ConvertAsNegative();
+				File.WriteAllLines("image.txt", rows.Select(r => new string(r)));
+
+
+				Console.SetCursorPosition(0, 0);
+			}
+		}
+
+
+		private static Bitmap ResizeBitmap(Bitmap bitmap)
+		{
+			var newHeight = bitmap.Height / WIDTH_OFFSET * MAX_WIDTH / bitmap.Width;
+
+			if (bitmap.Width > MAX_WIDTH || bitmap.Height > newHeight)
+			{
+				bitmap = new Bitmap(bitmap, new Size(MAX_WIDTH, (int)newHeight));
+			}
+
+			return bitmap;
+		}
+	}
+}
