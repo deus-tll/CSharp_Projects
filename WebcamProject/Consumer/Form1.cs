965d087b907504232360d347ba8cd9470ec369d7
deus-tll <millerartemka14@gmail.com>
2022-11-11 22:06:56 +0200

    update

---
+++ WebcamProject/Consumer/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Consumer
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private async void Form1_Load(object sender, EventArgs e)
+		{
+			var port = int.Parse(ConfigurationManager.AppSettings.Get("port"));
+			var client = new UdpClient(port);
+
+			while (true)
+			{
+				var data = await client.ReceiveAsync();
+
+				using (var ms = new MemoryStream(data.Buffer))
+				{
+					pictureBox1.Image = new Bitmap(ms);
+				}
+				Text = $"Bytes received: {data.Buffer.Length * sizeof(byte)}";
+			}
+		}
+
+		private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			var host = Dns.GetHostEntry(Dns.GetHostName());
+			MessageBox.Show(string.Join("\n", host.AddressList.
+				Where(i => i.AddressFamily == AddressFamily.InterNetwork)));
+		}
+	}
+}
